60710d60ceda8f71de7e3241e712da54ab90d833
LucaDalRi <euro2xd@gmail.com>
2023-02-06 18:09:26 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import NomeApp from './components/HelloWorld.vue';
+
+export default {
+  name: "App",
+  components: {
+    NomeApp,
+  }
+};
+</script>
+
+<template>
+
+  <NomeApp />
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'NomeApp',
+}
+</script>
+
+<template>
+
+  <div>
+    <h1>
+      Prova!
+    </h1>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
